bcc7973cb1b87672cbbdb9ff53f26053d3763a9c
caroline <caroline@debian.caroline>
2019-06-04 19:17:16 -0300

    eh pra funfar

--- app/src/main/java/com/example/apptorcedores/Info.java
+++ app/src/main/java/com/example/apptorcedores/Info.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 
 public class Info extends AppCompatActivity {
 
+    ArrayList<String> dados1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,40 @@ public class Info extends AppCompatActivity {
 
     public void listView() {
         ListView listview = (ListView) findViewById(R.id.listView);
+        dados1 = new ArrayList<>();
+        infoJogadores();
         String[] dados = new String[] { "https://www.dfilitto.com.br", "teste", "teste2"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados1);
         listview.setAdapter(adapter);
     }
 
+
+    public void infoJogadores() {
+        String Jogador1 = "Nome: Fábio Gonçalves dos Santos" + "\n" +  "Goleiro" + "\n" + "Estreia no time: Estreou contra a Internacional (no Major José Levy Sobrinho, em Limeira) no dia 19 de janeiro de 2019, em partida válida pelo Paulistão A2, e o jogo terminou com vitória do XV por 3 a 2.";
+        String Jogador2 = "Nome: Jefferson da Silva Luciano" + "\n" +  "Lateral-direito" + "\n" + "Estreia no time: Estreou contra a Internacional (no Major José Levy Sobrinho, em Limeira) no dia 19 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 3 a 2";
+        String Jogador3 = "Nome: Anderson Santos da Vitória" + "\n" +  "Lateral-esquerdo" + "\n" + "Estreia no time: Estreou contra o Juventus (no Barão da Serra Negra, em Piracicaba) no dia 4 de abril de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 1 a 0";
+        String Jogador4 = "Nome: Douglas Marques dos Santos" + "\n" +  "Zagueiro" + "\n" + "Estreia no time: Estreou contra a Internacional (no Major José Levy Sobrinho, em Limeira) no dia 19 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 3 a 2";
+        String Jogador5 = "Nome: Paulo Eduardo Rodrigues Lima" + "\n" +  "Zagueiro" + "\n" + "Estreia no time: Estreou contra o Juventus (no Barão da Serra Negra, em Piracicaba) no dia 4 de abril de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 1 a 0";
+        String Jogador6 = "Nome: Francisco Elias Eufrásio" + "\n" +  "Volante" + "\n" + "Estreia no time: Estreou contra a Portuguesa (no Barão da Serra Negra, em Piracicaba) no dia 23 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 2 a 0";
+        String Jogador11 = "Nome: Bruno Felipe Lima Teixeira" + "\n" +  "Volante" + "\n" + "Estreia no time: Estreou contra a Portuguesa (no Barão da Serra Negra, em Piracicaba) no dia 23 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 2 a 0";
+        String Jogador7 = "Nome: Cássio Gabriel Vilela Ferreira" + "\n" +  "Meio-campista" + "\n" + "Estreia no time: Estreou contra o Noroeste (no Dr. Alfredo de Castilho, em Bauru) no dia 8 de agosto de 2018, em partida válida pela Copa Paulista, e o jogo terminou empatado em 1 a 1";
+        String Jogador8 = "Nome: Danilo Petrolli Bueno" + "\n" +  "Meio-campista" + "\n" + "Estreia no time: Estreou contra a Internacional (no Major José Levy Sobrinho, em Limeira) no dia 19 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 3 a 2";
+        String Jogador9 = "Nome: Carlos Eduardo Barone Barboza" + "\n" +  "Atacante" + "\n" + "Estreia no time: Estreou contra a Internacional (no Major José Levy Sobrinho, em Limeira) no dia 19 de janeiro de 2019, em partida válida pelo Campeonato Paulista Série A2, e o jogo terminou com vitória do XV por 3 a 2";
+        String Jogador10 = "Nome: Luiz Henrique Mendes Silva da Costa" + "\n" +  "Atacante" + "\n" + "Estreia no time: Estreou contra a Portuguesa Santista (no Ulrico Mursa, em Santos) no dia 13 de fevereiro de 2019, em partida válida pelo Paulistão A2, e o jogo terminou com vitória do adversário por 4 a 3";
+
+        dados1.add(Jogador1);
+        dados1.add(Jogador2);
+        dados1.add(Jogador3);
+        dados1.add(Jogador4);
+        dados1.add(Jogador5);
+        dados1.add(Jogador6);
+        dados1.add(Jogador7);
+        dados1.add(Jogador8);
+        dados1.add(Jogador9);
+        dados1.add(Jogador10);
+        dados1.add(Jogador11);
+
+    }
 
 
     public void telaPrincipal(View view) {
